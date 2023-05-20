7a00ed6491e52f90180d634441201007a6f7a53b
demagleb <demagleb@mail.ru>
2023-05-21 00:28:32 +0300

    Auto find qt installed by conan

--- build.sh
+++ build.sh
@@ -1,23 +1,31 @@
 #!/bin/bash
 
+find_qt() {
+    grep "\[rootpath_qt\]" ./conanbuildinfo.txt --after-context=1 | tail -1
+}
 
 build_debug() {
-    # Debug QT
-    QMAKE="/home/demkogg/.conan/data/qt/5.15.7/_/_/package/967f72a50ebf83cb9bbefcddf520d319c6a6b084/bin/qmake"
+    # Debug
     mkdir -p build-debug
     cd build-debug
     conan install -s build_type=Debug --build=missing '../conanfile.txt' -s compiler.runtime=MT
+    QT="$(find_qt)"
+    QMAKE="${QT}/bin/qmake"
+    echo "using ${QMAKE}"
     $QMAKE ../TypingAnalysis.pro -spec linux-g++ CONFIG+=debug CONFIG+=qml_debug && make qmake_all
     make -j12 1> stdout-build.log 2> stderr-build.log
 }
 
 build_release() {
-    # Release QT
-    QMAKE="/home/demkogg/.conan/data/qt/5.15.7/_/_/package/100ace4f5c988ff9dec2ad4fd44dabdabc277a78/bin/qmake"
+    # Release
     mkdir -p build-release
     cd build-release
-    $QMAKE ../TypingAnalysis.pro -spec linux-g++ CONFIG+=qml_debug && make qmake_all
     conan install -s build_type=Release --build=missing '../conanfile.txt' -s compiler.runtime=MT
+    QT="$(find_qt)"
+    QMAKE="${QT}/bin/qmake"
+    echo "using ${QMAKE}"
+    $QMAKE ../TypingAnalysis.pro -spec linux-g++ CONFIG+=qml_debug && make qmake_all
+
     make -j12 1> stdout-build.log 2> stderr-build.log
 }
 
